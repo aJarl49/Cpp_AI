2cd28ada86a75253f79ae9e93ee4b889b50ee41a
SpiritTheUnicorn <jarl.agnieszka@gmail.com>
2025-09-12 21:52:55 +0200

    first commit
    w/out venv

---
+++ syntax_variables.cpp
@@ -0,0 +1,6 @@
+#include <iostream> //input output stream
+
+int main() {
+    std::cout << "Hello World";  //standard character out
+    return 0; // program is going to terminate correctly - if it's anything else, there is an error
+}
