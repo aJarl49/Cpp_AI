219b6b22649de40eb0e438d4391452b763d777d2
SpiritTheUnicorn <jarl.agnieszka@gmail.com>
2025-09-13 23:08:25 +0200

    structure
    setting folders for each chapter

--- syntax_variables/assignment_1.cpp
+++ syntax_variables/assignment_1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main (){
 
-    int num_1;
+    int num_1; // whatif int num_1=0, num_2=0
     int num_2;
     int add;
     int sub;
@@ -16,13 +16,13 @@ int main (){
     cin>>num_1;
     cout<<"second number: ";
     cin>>num_2;
-    add=num_1+num_2;
+    add=num_1+num_2; // whatif this part gone
     sub=num_1-num_2;
     mul=num_1*num_2;
     div=num_1/num_2;
-    cout<<"Your numbers added give "<<add<<std::endl;
+    cout<<"Your numbers added give "<<add<<std::endl; //std::endl for jumping to the new line
     cout<<"Your numbers subtracted give "<<sub<<std::endl;
-    cout<<"Your numbers multiplied give "<<mul<<std::endl;
+    cout<<"Your numbers multiplied give "<<mul<<std::endl; //whatif cout<<str<<num_*num_2<<std::endl;
     cout<<"Your number divided give "<<div<<std::endl;
     return 0;
 } // cannot edit in read-only editor: how to run it interminal?
