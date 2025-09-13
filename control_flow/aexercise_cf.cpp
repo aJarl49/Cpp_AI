9bc6434e8da85cde931e89b197bf6e5d0cfa6dbd
SpiritTheUnicorn <jarl.agnieszka@gmail.com>
2025-09-14 01:25:56 +0200

    control flow part one
    skelet for control flow assignment

---
+++ control_flow/aexercise_cf.cpp
@@ -0,0 +1,13 @@
+//Exercise: Write a function that squares a number.
+
+# include <iostream>
+using namespace std;
+
+int main (){
+
+    int num_1=0;
+    std::cout<<"Enter a number :"<<std::endl;
+    std::cin>>num_1;
+    std::cout<<"Your number squared is "<<num_1*num_1<<std::endl;
+    return 0;
+}
---
+++ control_flow/assignment.cpp
@@ -0,0 +1,17 @@
+// Implement a calculator that takes user input and applies different operations (add, subtract, etc.).
+
+#include <iostream>
+using namespace std;
+
+
+//int add (int, int)
+int main(){
+    int num_1, num_2;
+    std::cout<<"Enter two numbers separated by space: "<<std::endl;
+    std::cin>>num_1>>num_2; // no std::endl here
+    //std::cout<<"addition result is <<add(num_1,num_2)"
+
+    return 0;
+}
+
+//int add(int num_1, int num_2){return num_1+num_2}
