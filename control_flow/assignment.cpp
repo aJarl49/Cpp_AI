d66f8047618293e0f161edee1417c477397c13d3
SpiritTheUnicorn <jarl.agnieszka@gmail.com>
2025-09-14 15:36:14 +0200

    pt2
    save for diagnostic of cache files

--- control_flow/assignment.cpp
+++ control_flow/assignment.cpp
@@ -4,14 +4,18 @@
 using namespace std;
 
 
-//int add (int, int)
+int add (int, int);
+
 int main(){
-    int num_1, num_2;
+    int num_1=0, num_2=0;
     std::cout<<"Enter two numbers separated by space: "<<std::endl;
-    std::cin>>num_1>>num_2; // no std::endl here
-    //std::cout<<"addition result is <<add(num_1,num_2)"
+    std::cin>>num_1, num_2; // no std::endl here
+    std::cout<<num_1<<std::endl;
+    std::cout<<"addition result is " <<add(num_1,num_2)<<std::endl;
 
     return 0;
 }
 
-//int add(int num_1, int num_2){return num_1+num_2}
+int add(int num_1, int num_2){
+    return num_1+num_2;
+}// 
